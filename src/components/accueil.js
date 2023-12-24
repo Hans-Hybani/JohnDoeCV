dde0cf88cc86da009ccb96fc980df098dd0122ab
Hans-Hybani <146996083+Hans-Hybani@users.noreply.github.com>
2023-12-24 22:15:44 +0100

    Création de la carte avec les compétences ainsi que le responsive de cette partie

--- src/components/accueil.js
+++ src/components/accueil.js
@@ -1,5 +1,6 @@
 import React from "react";
 import softwaredeveloper from '../assets/softwaredeveloper.jpg'
+import developpeurBureau from '../assets/developpeurBureau.jpg'
 import '../styles/accueil.css'
 import '../styles/Button.css'
 import Button from "./button";
@@ -11,11 +12,56 @@ function Accueil(props) {
         return(
                 <div>
                         <div className="img__conteneur">
-                                <img src={softwaredeveloper} alt='fond' className="img__image"/>
+                                <img src={softwaredeveloper} alt='fond ecran' className="img__image"/>
                                 <h1 className="h1__conteneur">{titre}</h1>
                                 <h2 className="h2__conteneur">{sousTitre}</h2>
                                 {/* <button className="all__button">En savoir plus</button> */}
                                 <Button className="all__button"/>
+                        </div>
+                        <div className="apropos__conteneur">
+                                <div className="apropos__conteneur__carte">
+                                        <div className="apropos__conteneur__carte__text">
+                                                <h3 className="apropos__titre">A propos</h3>
+                                                <hr className="bare"></hr>
+                                                <p className="apropos__paragraph">Passioné par l'informatique et les nouvelles <br></br>
+                                                technologies, j'ai suivi une formation d'<strong>intégrateur développeur web</strong> au CEF. Au cours de cette formation,<br></br>
+                                                j'ai pu acquérir des bases solides pour travailler dans le<br></br>
+                                                domaine du <strong>développement web.</strong></p>
+
+                                                <p>Basé à Lyon, je suis en recherche d'une alternance au<br></br>
+                                                sein d'une agence digitale pour consolider ma formation<br></br>
+                                                de <strong>développeur web full stack.</strong></p>
+
+                                                <p>J'accorde une attention particulière à la qualité du code<br></br>
+                                                que j'écris et je respecte les bonnes pratiques du web.</p>
+                                        </div>
+                                        <div className="apropos__conteneur__carte__image">
+                                                <img src={developpeurBureau} alt='developpeur' className="img__dev"/>
+                                                <h4 className="competence">Mes compétences</h4>
+                                                <div className="competence__porcentage">
+                                                        <div className="competence__porcentage__bloc">
+                                                                HTLM5 90%
+                                                                <div className="bar"><div className="html"></div></div>
+                                                        </div>
+                                                        <div className="competence__porcentage__bloc">
+                                                                CSS3 80%
+                                                                <div className="bar"><div className="css"></div></div>
+                                                        </div>
+                                                        <div className="competence__porcentage__bloc">
+                                                                JAVASCRIPT 70%
+                                                                <div className="bar"><div className="js"></div></div>
+                                                        </div>
+                                                        <div className="competence__porcentage__bloc"> 
+                                                                PHP 60%
+                                                                <div className="bar"><div className="php"></div></div>
+                                                        </div>
+                                                        <div className="competence__porcentage__bloc"> 
+                                                                REACT 50%
+                                                                <div className="bar"><div className="react"></div></div>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
                         </div>
                 </div>
         )
